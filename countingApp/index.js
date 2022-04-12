d46da5bf655729d81bc095fe415907afee8810a6
davine94 <davine94>
2022-04-12 15:34:16 +0300

    Functionality added to the button

--- countingApp/index.js
+++ countingApp/index.js
@@ -1,7 +1,13 @@
+let countEl= document.getElementById("count-el");
+console.log(countEl);
 //initialize the count as 0
+let count =0;
 //listen for clicks on the increment button
 function increment(){
-    console.log("The button works")
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
 }
-//increment the count variable when the button is clicked
+
+
 //change the count-el in the HTML to reflect the count

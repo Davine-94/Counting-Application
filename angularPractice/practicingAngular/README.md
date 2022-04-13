f8c219d7e6d6753a70d0d0f64fca768366b1e71b
davine94 <davine94>
2022-04-14 00:17:55 +0300

    Learning property binding

--- angularPractice/practicingAngular/README.md
+++ angularPractice/practicingAngular/README.md
@@ -1,4 +1,5 @@
 # PracticingAngular
+This is a project by Davine Phylis that shows my progress in learning angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.1.
 
--- angularPractice/practicingAngular/src/app/practice/practice.component.ts
+++ angularPractice/practicingAngular/src/app/practice/practice.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PracticeComponent implements OnInit {
 
+  public name = "Davine";
+  public myId = "practiceId";
+  public isDisabled = "true";
   constructor() { }
 
   ngOnInit(): void {

d2454f0b49843ab70535494f8323e3865576938f
davine94 <davine94>
2022-04-14 05:51:23 +0300

    Practiced class binding

--- angularPractice/practicingAngular/src/app/practice/practice.component.ts
+++ angularPractice/practicingAngular/src/app/practice/practice.component.ts
@@ -10,6 +10,14 @@ export class PracticeComponent implements OnInit {
   public name = "Davine";
   public myId = "practiceId";
   public isDisabled = "true";
+  public colorText = "text-success";
+  public hasError = "text-danger";
+  public isSpecial = true;
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger":this.hasError,
+    "text-special": this.isSpecial
+  }
   constructor() { }
 
   ngOnInit(): void {
